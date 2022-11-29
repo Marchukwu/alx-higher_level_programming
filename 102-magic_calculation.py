e1e069e532781c0c97d9543e3ef8ee2be4237f79
Mara <115796898+Marchukwu@users.noreply.github.com>
2022-11-29 07:50:45 +0100

    Add files via upload

--- 102-magic_calculation.py
+++ 102-magic_calculation.py
@@ -1,2 +1,10 @@
-def magic_calculation(a, b):
-    return (98 + a ** b)
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
